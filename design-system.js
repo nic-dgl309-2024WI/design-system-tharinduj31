c94e7591e4579d62ea378b799fea622b1b1455de
Mudduwe Jayasekara <tharinduj31@gmail.com>
2024-02-04 14:18:14 -0800

    Add hamburger option

---
+++ design-system.js
@@ -0,0 +1,7 @@
+function showNav() {
+    var element = document.getElementById("nav-items");
+    element.classList.toggle("show-items");
+    
+    var ele = document.getElementById("aside-items");
+    ele.classList.toggle("show-items");
+  }
