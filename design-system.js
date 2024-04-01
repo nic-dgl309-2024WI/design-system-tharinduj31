0e13ad35fe996ea94d8f85ce9e322ae787a02f24
Mudduwe Jayasekara <tharinduj31@gmail.com>
2024-04-01 01:27:33 -0700

    Add accordion to product mobile view

--- design-system.js
+++ design-system.js
@@ -20,3 +20,51 @@ function showNav() {
       imageToShow.style.display = 'inline-block';
     }
   }
+
+  var hiddenMobileCards = []; // Array to store hidden mobile cards
+
+  // Function to check if the viewport width is below a certain threshold (considered as mobile)
+  function isMobileViewport() {
+    return window.innerWidth < 800; // Adjust the threshold as needed
+  }
+  
+  // Hide the "View Less" button initially only if in mobile view
+  if (isMobileViewport()) {
+    document.getElementById('button-view-less').style.display = 'none';
+  }
+  
+  document.getElementById('button-view-more').addEventListener('click', function() {
+    var mobileCards = document.querySelectorAll('.c-card--mobile');
+    mobileCards.forEach(function(card) {
+      hiddenMobileCards.push(card); // Store reference to hidden mobile cards
+      card.classList.remove('c-card--mobile');
+    });
+    document.getElementById('button-view-more').style.display = 'none'; // Hide "View More" button
+    document.getElementById('button-view-less').style.display = 'block'; // Show "View Less" button
+  });
+  
+  document.getElementById('button-view-less').addEventListener('click', function() {
+    hiddenMobileCards.forEach(function(card) {
+      card.classList.add('c-card--mobile'); // Add back the c-card--mobile class
+    });
+    hiddenMobileCards = []; // Clear the array after showing the cards
+  
+    document.getElementById('button-view-less').style.display = 'none'; // Hide "View Less" button
+    document.getElementById('button-view-more').style.display = 'block'; // Show "View More" button
+  });
+  
+  
+  
+  
+
+
+ 
+  
+  
+  
+  
+
+
+
+
+  
